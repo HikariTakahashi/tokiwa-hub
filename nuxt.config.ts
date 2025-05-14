84e711a4a5ab3a67c402237fea8c93b822da20b0
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-05-14 20:53:39 +0900

    install/tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/icon", "@kgierke/nuxt-basic-auth"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@kgierke/nuxt-basic-auth",
+    "@nuxtjs/tailwindcss",
+  ],
   basicAuth: {
     enabled: true,
     users: [
