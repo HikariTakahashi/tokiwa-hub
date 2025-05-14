c42b2cc2ee173f515f635e974453a61d4932f3a4
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-05-14 11:30:36 +0900

    install/BasicAuth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
-})
+  modules: ["@nuxt/eslint", "@nuxt/icon", "@kgierke/nuxt-basic-auth"],
+  basicAuth: {
+    enabled: true,
+    users: [
+      {
+        username: process.env.BASIC_AUTH_USERNAME || "",
+        password: process.env.BASIC_AUTH_PASSWORD || "",
+      },
+    ],
+  },
+});
