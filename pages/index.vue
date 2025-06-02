157770b25aec0873f4820a6d53f750c02af9249e
Hikari Takahashi <166400513+HikariTakahashi@users.noreply.github.com>
2025-06-02 09:16:00 +0900

    update/index.vue/GitHubIssues

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,31 @@
 <template>
   <div class="container mx-auto px-4 py-8">
     <h1 class="text-3xl font-bold mb-8">TokiWa hub(試験版)</h1>
-    <GitHubIssues />
+    <GitHubIssues
+      name="tokiwa-calendar-frontend"
+      owner="HikariTakahashi"
+      link="https://github.com/HikariTakahashi/tokiwa-calendar-frontend"
+    />
+    <GitHubIssues
+      name="tokiwa-calendar-backend"
+      owner="HikariTakahashi"
+      link="https://github.com/HikariTakahashi/tokiwa-calendar-backend"
+    />
+    <GitHubIssues
+      name="tokiwa-calendar-hardware"
+      owner="HikariTakahashi"
+      link="https://github.com/HikariTakahashi/tokiwa-calendar-hardware"
+    />
+    <GitHubIssues
+      name="tokiwa-calendar-discordbot"
+      owner="HikariTakahashi"
+      link="https://github.com/HikariTakahashi/tokiwa-calendar-discordbot"
+    />
+    <GitHubIssues
+      name="tokiwa-hub"
+      owner="HikariTakahashi"
+      link="https://github.com/HikariTakahashi/tokiwa-hub"
+    />
   </div>
 </template>
 
